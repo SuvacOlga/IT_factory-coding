627fd21266b79bae94cdfa019c6394a937e9e0da
Olga <89861023+SuvacOlga@users.noreply.github.com>
2022-03-16 11:02:01 +0100

    Create test_edition.py

---
+++ Pytest_demos/test_edition.py
@@ -0,0 +1,13 @@
+def sum_two(num1, num2):
+    return num1 + num2
+
+
+def test_sum_1():
+    assert sum_two(2, 5) == 7
+
+
+def test_sum_2():
+    assert sum_two(2, 5) != 4
+© 2022 GitHub, Inc.
+Terms
+Privacy
